f50bd31880968b54bf2ef654367003ec1479915e
Reza Madani <rezamadani.ai@gmail.com>
2025-03-12 19:33:11 +0100

    Update

--- importance_score_evaluator/utils.py
+++ importance_score_evaluator/utils.py
@@ -170,7 +170,7 @@ def get_rationales(model, tokenizer, prompt, norm='inf', mode='prob'):
         b, e = t_range
         high = 1.0
         low = 0.0
-        for _ in range(10):  # with 10 iteration the precision would be 2e-10 ~= 0.001
+        for _ in range(5):  # with 10 iteration the precision would be 2e-10 ~= 0.001
             k = (low + high) / 2
             with torch.no_grad():
                 low_scores = make_noisy_embeddings(model, inp, norm=norm, tokens_to_mix=t_range, scale=k)
@@ -200,19 +200,7 @@ def get_rationales(model, tokenizer, prompt, norm='inf', mode='prob'):
     # tokens_score = tokens_score - torch.min(tokens_score)
     tokens_score = tokens_score / torch.sum(tokens_score)
 
-    # Aggregate word scores by averaging sub-tokens scores
-    word_scores = torch.tensor([
-        tokens_score[b:e].sum().item() for b, e in tokens_range
-    ], device=device)
-
-    # Consider removing softmax if raw scores are preferred
-    # word_scores = torch.softmax(word_scores, dim=-1)
-
     return tokens_score.unsqueeze(0)
-    # {
-    #     'word_scores': word_scores,
-    #     'token_scores': tokens_score.unsqueeze(0)
-    # }
 
 def recursive_copy(x, clone=None, detach=None, retain_grad=None):
     """
